b2d9e6accd6d80f23242ec7765ad30635e5d1418
Abhishek Dhole <52071318+abhishekdhole@users.noreply.github.com>
2022-01-24 22:52:29 +0530

    Add files via upload

---
+++ PassingExample.java
@@ -0,0 +1,12 @@
+package com.abhishek;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String  name= "Abhishek Dhole";
+        greet(name);
+    }
+    static void greet(String name) {
+        System.out.println(name);
+    }
+}
+

9e4070d8cfe05af980d78e268c5f03e7b0e13c75
Abhishek Dhole <52071318+abhishekdhole@users.noreply.github.com>
2022-01-26 21:50:27 +0530

    Add files via upload

---
+++ ChangeValue.java
@@ -0,0 +1,19 @@
+package com.abhishek;
+
+import java.util.Arrays;
+
+public class ChangeValue {
+    public static void main(String[] args) {
+        //create an array
+        int[] arr ={ 1,2,3,4,5};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static  void change(int[] nums){
+        nums[0]=99;//if you make a change to the  object via this reference variable,the same object will be changed.
+
+
+    }
+}
---
+++ Scope.java
@@ -0,0 +1,17 @@
+package com.abhishek;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a=10;
+        int b= 30;
+
+        System.out.println(marks);
+
+
+    }
+    static  void  random(int marks){
+        int num =89;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
